9f99c7417ca0f49122a92726587cfc73894186ad
Julieta Guareschi <juliguareschi@gmail.com>
2023-11-28 21:42:37 -0300

    main

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function saludar() {
+    console.log("Hola a todos");
+}
+
+saludar();
+
+
+function solicitarMostrarNombre(){
+    let ingresarNombre = prompt("Ingresar Nombre");
+    console.log("El nombre es " + ingresarNombre);
+}
+
+solicitarMostrarNombre();
+solicitarMostrarNombre();
